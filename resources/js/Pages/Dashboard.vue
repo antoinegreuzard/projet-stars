de14c16e1f360554458d745f38cd5030d3c22d96
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-02-13 14:33:48 +0100

    chore: add stars component

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/vue3';
+import StarsTable from "@/Components/StarsTable.vue";
 </script>
 
 <template>
@@ -13,12 +14,12 @@ import {Head} from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6">
                     <div class="p-6 text-gray-900">You're logged in!</div>
                 </div>
+
+                <StarsTable/>
             </div>
         </div>
-
-        <StarsTable/>
     </AuthenticatedLayout>
 </template>
