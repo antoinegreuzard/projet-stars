df968d659010076ef26fae9edc7cc49f8c6b5843
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-02-21 13:44:12 +0100

    fix: translate fr > en

--- resources/js/Components/StarsTable.vue
+++ resources/js/Components/StarsTable.vue
@@ -87,13 +87,13 @@
                     @click="handleSubmitEdit"
                     class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
-                    Modifier
+                    Save
                 </button>
                 <button
                     @click="cancelEdit"
                     class="btn bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
                 >
-                    Annuler
+                    Cancel
                 </button>
             </div>
         </div>
