f566fd9820145db1205f16f19b8cebdf804cc100
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-05-22 19:59:45 +0200

    test

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Laravel\Sanctum\Sanctum;
 
 return [
@@ -15,11 +16,13 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    'stateful' => tap(explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
-    ))),
+    ))), function ($domains) {
+        Log::info('Sanctum stateful domains:', $domains);
+    }),
 
     /*
     |--------------------------------------------------------------------------
@@ -79,5 +82,4 @@ return [
         'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
     ],
-
 ];
