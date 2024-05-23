cd7f843ea5df1a1e7841da454e47b24b4a9eb2ae
Antoine Greuzard <antoine@antoinegreuzard.fr>
2024-05-23 14:42:01 +0200

    Update axiosConfig.js
    Signed-off-by: Antoine Greuzard <antoine@antoinegreuzard.fr>

--- resources/js/axiosConfig.js
+++ resources/js/axiosConfig.js
@@ -3,13 +3,4 @@ import axios from 'axios'
 axios.defaults.withCredentials = true
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-// Function to fetch the CSRF token
-export const fetchCsrfToken = async () => {
-    try {
-        await axios.get('/sanctum/csrf-cookie')
-    } catch (error) {
-        console.error('Error fetching CSRF token:', error)
-    }
-}
-
 export default axios
