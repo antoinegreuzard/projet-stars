b23719552efa9acc7284228b1a006bf129a214a8
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-05-24 10:51:04 +0200

    fix: ziggy call

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,6 +1,6 @@
 import { PageProps as InertiaPageProps } from '@inertiajs/core'
 import { AxiosInstance } from 'axios'
-import ziggyRoute from 'ziggy-js'
+import { route as ziggyRoute } from 'ziggy-js'
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import { PageProps as AppPageProps } from '.'
 
