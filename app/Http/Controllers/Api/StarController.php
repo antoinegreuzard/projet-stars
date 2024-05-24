ca850abc0a92186ba777cca68538931f219d60ca
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-05-24 19:51:47 +0200

    fix: correct way to use sanctum with swagger

--- app/Http/Controllers/Api/StarController.php
+++ app/Http/Controllers/Api/StarController.php
@@ -9,70 +9,13 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 /**
- * @OA\OpenApi(
- *    @OA\Info(
- *        title="My API",
- *        version="1.0.0",
- *        description="API for managing stars",
- *        @OA\Contact(
- *            email="antoine@antoinegreuzard.fr"
- *        )
- *    ),
- *    @OA\Server(
- *        description="API Server",
- *        url=L5_SWAGGER_CONST_HOST
- *    ),
- *    @OA\Components(
- *        @OA\Schema(
- *            schema="Star",
- *            type="object",
- *            required={"name", "first_name"},
- *            @OA\Property(
- *                property="id",
- *                type="integer",
- *                format="int64",
- *                description="Unique identifier for the Star"
- *            ),
- *            @OA\Property(
- *                property="name",
- *                type="string",
- *                description="Name of the Star"
- *            ),
- *            @OA\Property(
- *                property="first_name",
- *                type="string",
- *                description="First name of the Star"
- *            ),
- *            @OA\Property(
- *                property="image",
- *                type="string",
- *                format="uri",
- *                description="URL of the Star's image"
- *            ),
- *            @OA\Property(
- *                property="description",
- *                type="string",
- *                description="Description of the Star"
- *            )
- *        ),
- *        @OA\SecurityScheme(
- *            securityScheme="sanctum",
- *            type="apiKey",
- *            name="Authorization",
- *            in="header",
- *            description="Enter token in format (Bearer <token>)"
- *        )
- *    )
+ * @OA\Tag(
+ *     name="Stars",
+ *     description="Star endpoints"
  * )
  */
 class StarController extends Controller
 {
-    public function __construct()
-    {
-        // Appliquez le middleware 'auth:sanctum' à seulement certaines méthodes
-        $this->middleware('auth:sanctum')->only(['store', 'update', 'destroy']);
-    }
-
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,63 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\OpenApi(
+ *    @OA\Info(
+ *        title="My API",
+ *        version="1.0.0",
+ *        description="API for managing stars",
+ *        @OA\Contact(
+ *            email="antoine@antoinegreuzard.fr"
+ *        )
+ *    ),
+ *    @OA\Server(
+ *        description="API Server",
+ *        url=L5_SWAGGER_CONST_HOST
+ *    ),
+ *    @OA\Components(
+ *        @OA\Schema(
+ *            schema="Star",
+ *            type="object",
+ *            required={"name", "first_name"},
+ *            @OA\Property(
+ *                property="id",
+ *                type="integer",
+ *                format="int64",
+ *                description="Unique identifier for the Star"
+ *            ),
+ *            @OA\Property(
+ *                property="name",
+ *                type="string",
+ *                description="Name of the Star"
+ *            ),
+ *            @OA\Property(
+ *                property="first_name",
+ *                type="string",
+ *                description="First name of the Star"
+ *            ),
+ *            @OA\Property(
+ *                property="image",
+ *                type="string",
+ *                format="uri",
+ *                description="URL of the Star's image"
+ *            ),
+ *            @OA\Property(
+ *                property="description",
+ *                type="string",
+ *                description="Description of the Star"
+ *            )
+ *        ),
+ *        @OA\SecurityScheme(
+ *            securityScheme="sanctum",
+ *            type="apiKey",
+ *            name="Authorization",
+ *            in="header",
+ *            description="Enter token in format (Bearer <token>)"
+ *        )
+ *    )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests;
